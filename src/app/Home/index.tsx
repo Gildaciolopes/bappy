e3f4cd2f83b218a0a4d5e432d7777163e10e19d4
Gildácio Lopes <gildacioj399@gmail.com>
2025-10-24 19:52:03 -0300

    feat: add Item component for displaying tasks with status and remove functionality

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -3,6 +3,7 @@ import { View, Image, TouchableOpacity, Text } from "react-native";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { Filter } from "@/components/Filter";
+import { Item } from "@/components/Item";
 
 import { styles } from "./styles";
 import { FilterStatus } from "@/types/FilterStatus";
@@ -28,6 +29,12 @@ export function Home() {
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
+
+        <Item
+          data={{ status: FilterStatus.DONE, description: "Café" }}
+          onChangeStatus={() => console.log("mudar status")}
+          onRemove={() => console.log("remover")}
+        ></Item>
       </View>
     </View>
   );
