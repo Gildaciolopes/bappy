7ea384808531928f877ac31d0b5eac0dd884a646
Gildácio Lopes <gildacioj399@gmail.com>
2025-10-24 16:37:11 -0300

    feat: add Filter component and integrate it into Home screen

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -2,8 +2,10 @@ import { View, Image } from "react-native";
 
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
+import { Filter } from "@/components/Filter";
 
 import { styles } from "./styles";
+import { FilterStatus } from "@/types/FilterStatus";
 
 export function Home() {
   return (
@@ -14,7 +16,10 @@ export function Home() {
         <Button title="Adicionar" />
       </View>
 
-      <View style={styles.content}></View>
+      <View style={styles.content}>
+        <Filter status={FilterStatus.DONE} isActive />
+        <Filter status={FilterStatus.PENDING} isActive={false} />
+      </View>
     </View>
   );
 }
