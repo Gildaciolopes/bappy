e0933f1b7a45914b7a1b2456aede287a59decb87
Gildácio Lopes <gildacioj399@gmail.com>
2025-10-24 11:39:25 -0300

    feat: extend Button component to accept additional TouchableOpacity props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,14 @@
-import { TouchableOpacity, Text } from "react-native";
+import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
 
 import { styles } from "./styles";
 
-type Props = {
+type Props = TouchableOpacityProps & {
   title: string;
 };
 
-export function Button({ title }: Props) {
+export function Button({ title, ...rest }: Props) {
   return (
-    <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+    <TouchableOpacity style={styles.button} activeOpacity={0.8} {...rest}>
       <Text style={styles.buttonText}>{title}</Text>
     </TouchableOpacity>
   );
